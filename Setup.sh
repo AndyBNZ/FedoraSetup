4308a02343aa01209432bb8e5dd12fe7399bfc1a
AndyBNZ <37642234+AndyBNZ@users.noreply.github.com>
2022-05-30 21:25:30 +1000

    Add files via upload

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,16 @@
+read -p "Starting environment swap, press any key to resume ..."
 sudo dnf swap @server-product-environment @minimal-environment -y
+read -p "Starting pantheon install, press any key to resume ..."
 sudo dnf group install ‘Pantheon Desktop’ -y
+
+read -p "Disabling GDM, press any key to resume ..."
 systemctl disable gdm
+read -p "Enabling LightDM, press any key to resume ..."
 systemctl enable lightdm
+read -p "Setting default environment to graphical, press any key to resume ..."
 systemctl set-default graphical.target
+
+read -p "Uninstalling Pantheon bloat, press any key to resume ..."
 sudo dnf remove elementary-music -y 
 sudo dnf remove elementary-videos -y
 sudo dnf remove elementary-files -y
@@ -11,6 +19,8 @@ sudo dnf remove elementary-calendar -y
 sudo dnf remove elementary-code -y
 sudo dnf remove elementary-camera -y
 sudo dnf remove elementary-photos -y
+
+read -p "Installing preferred apps, press any key to resume ..."
 sudo dnf install dolphin -y
 sudo dnf install gnome-boxes -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -23,4 +33,6 @@ sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
+
+read -p "Press any key to reboot ..."
 reboot
